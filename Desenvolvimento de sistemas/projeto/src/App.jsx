330a9a419255350ccbb000d88efde027ed43cc14
PEDRO GABRIEL CALEIROS SILVA <pedro-gabriel_silva@estudante.sesisenai.org.br>
2026-02-24 21:52:44 -0300

    Ala de Revisão de React

---
+++ Desenvolvimento de sistemas/projeto/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Bory from './layouts/Bory/Bory'
+import { Footer } from './layouts/Footer/Footer'
+import Header from './layouts/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header title="Identidade "/>
+      <Footer autor="Pedro Gabriel"/>
+      <Bory/>
+
+    </>
+  )
+}
+
+export default App
---
+++ Desenvolvimento de sistemas/projeto/src/layouts/Header/index.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+
+const Header = (props) => {
+  return (
+      <header className={styles.header}>
+        <h1 className={styles.title}>{props.title}</h1>
+      </header>
+  )
+}
+export default Header
+
+
