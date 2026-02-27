70832a684c8c6931e7900cc44cf9eaffcbf33c86
PedroCallieri <pedroddrro@gmail.com>
2026-02-26 21:33:51 -0300

    aula de Estrutura de Dados

---
+++ Desenvolvimento de sistemas/Professor/projeto/src/layouts/Bory/Bory.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../../componentes/Card'
+import styles from './Bory.modules.css'
+
+const Bory = () => {
+  return (
+    <>
+    <Card nome="Pedrão" idade="17 anos" cidade="Salvador🎄"/>
+    <Card nome="Filipão" idade="24 anos" cidade="Rio de Janeiro🎄"/>
+    <Card nome="Thiago" idade="32 anos" cidade="São Paulo🎄"/>
+    </>
+  )
+}
+
+export default Bory
