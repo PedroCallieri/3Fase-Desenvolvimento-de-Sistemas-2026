44ecff061a7393a6e56b7ada8359a9cfb27353cf
PedroCallieri <pedroddrro@gmail.com>
2026-03-03 22:02:46 -0300

    adicionei aula sobre react com professor lucio

---
+++ desi-senai-2026-1-main/desi-senai-2026-1-main/fundamentos/src/App.jsx
@@ -0,0 +1,19 @@
+import Body from "./layouts/Body/Body"
+import { Footer } from "./layouts/Footer/Footer"
+import Header from "./layouts/Header"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Header title="Proboll"/>
+      <Body/>
+      <Footer autor="Pedro Gabriel "/>
+
+    </>
+  )
+}
+
+export default App
