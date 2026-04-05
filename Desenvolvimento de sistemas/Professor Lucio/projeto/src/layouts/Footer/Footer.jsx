4a0531e5325ad7b5d123b48cac1ab2f5aa30c4ce
PedroCallieri <pedroddrro@gmail.com>
2026-04-05 19:48:25 -0300

    adicionei projeto frontEnd

--- Desenvolvimento de sistemas/Professor Lucio/projeto/src/layouts/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './Footer.module.css'
-
-export const Footer = ({autor}) => {
-  return (
-    <p className={styles.p}>Esse site foi desenvolvido por {autor}</p>
-  )
-}
